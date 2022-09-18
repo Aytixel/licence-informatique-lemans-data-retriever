0058fb56580723a5c61053c8bb6501eed3f4a330
Aytixel <aytixel@gmail.com>
2022-09-18 17:26:01 +0200

    Parse icalendar data

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,54 @@ const planning_db = mongo_client.database("planning-v2");
 const l1 = planning_db.collection<L1>("l1");
 */
 
+interface ToDate {
+  toJSDate: () => Date;
+}
+
+interface Event {
+  startDate: ToDate;
+  endDate: ToDate;
+  location: string;
+  summary: string;
+  description: string;
+}
+
+interface Course {
+  title: string;
+  start_date: Date;
+  end_date: Date;
+  description: string;
+  room: string;
+}
+
+interface Day {
+  date: Date;
+  group: number;
+  courses: Course[];
+}
+
 const env = config();
 const planning_raw_data = await retrieve(env);
+const plannings: any = {
+  l1: [],
+  l2: [],
+  l3: [],
+  m1: [],
+  m2: [],
+  salle_ic2: [],
+};
+
+/*
+first more recent   : +
+second more recent  : -
+first = second      : 0
+*/
+const compare_date = (first_date: Date, second_date: Date) =>
+  first_date.getTime() - second_date.getTime();
+
+// keep only the date part
+const keep_only_date = (date: Date) =>
+  new Date(date.getTime() - (date.getTime() % (1000 * 60 * 60 * 24)));
 
 for (const resource_type_key in planning_raw_data) {
   for (
@@ -31,10 +77,39 @@ for (const resource_type_key in planning_raw_data) {
     resource_id_index < planning_raw_data[resource_type_key].length;
     resource_id_index++
   ) {
-    console.log(
-      new ICAL.Component(
-        ICAL.parse(planning_raw_data[resource_type_key][resource_id_index]),
-      ).getAllSubcomponents("vevent"),
-    );
+    const planning: Day[] = [];
+    const events: Event[] = new ICAL.Component(
+      ICAL.parse(planning_raw_data[resource_type_key][resource_id_index]),
+    ).getAllSubcomponents("vevent").map((x: unknown) => new ICAL.Event(x));
+
+    events.forEach((event: Event) => {
+      const date = keep_only_date(event.startDate.toJSDate());
+      let day_index = planning.findIndex((x) => !compare_date(x.date, date));
+      const day = (day_index > -1) ? planning[day_index] : planning[
+        day_index = planning.push({
+          date: date,
+          group: resource_id_index,
+          courses: [],
+        }) - 1
+      ];
+
+      day.courses.push(
+        {
+          title: event.summary,
+          start_date: event.startDate.toJSDate(),
+          end_date: event.endDate.toJSDate(),
+          description: event.description.trim().split("\n").slice(0, -1).join(
+            "\n",
+          ),
+          room: event.location,
+        },
+      );
+
+      planning[day_index] = day;
+    });
+
+    plannings[resource_type_key].push(planning);
   }
 }
+
+console.log(plannings);
