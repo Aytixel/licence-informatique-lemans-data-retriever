8c794cc18b5ccf20539eb26d6755d0e7ab162050
Aytixel <aytixel@gmail.com>
2022-09-18 16:28:41 +0200

    Enable puppeteer headless mode

--- src/retriever.ts
+++ src/retriever.ts
@@ -4,7 +4,7 @@ import retriever from "./browser/retriever.js";
 
 export default async function (env: DotenvConfig) {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ["--no-sandbox", "--disable-dev-shm-usage"],
   });
   const page = await login(env, browser);
